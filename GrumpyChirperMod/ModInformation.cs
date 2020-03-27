93e706f2f6a796d6705ee56be3580396d4efd117
Russ Penska <russpenska@gmail.com>
2020-03-27 17:57:27 +0000

    Add project files.

---
+++ GrumpyChirperMod/ModInformation.cs
@@ -0,0 +1,10 @@
+﻿using ICities;
+
+namespace GrumpyChirperMod
+{
+    public class ModInformation : IUserMod
+    {
+        public string Name => "Grumpy Chirper Mod";
+        public string Description => "Adds grumpy and sarcastic messages to the Chirper!";
+    }
+}
