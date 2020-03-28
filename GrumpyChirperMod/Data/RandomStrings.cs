228e92dd4b306877f5528c060beb8140d68089e0
George Bell <george@cattlebell.com>
2020-03-28 18:28:17 +0000

    Moved random strings into their own class.

---
+++ GrumpyChirperMod/Data/RandomStrings.cs
@@ -0,0 +1,40 @@
+﻿namespace GrumpyChirperMod.Data
+{
+    public class RandomStrings
+    {
+        public static string[] Names() => new string[]
+        {
+            "Grumpy Bob",
+            "Dave The Grouch",
+            "Angry Rob",
+            "Irritated Irene"
+        };
+
+        public static string[] LoadingMessages() => new string[]
+        {
+            "Don't you have anything better to do? #sigh",
+            "Oh good the mayor's back. #giveup",
+            "Another beautiful day in the worst city ever!",
+            "BREAKING: The mayor unveils plans to somehow make things even worse.",
+            "Oh great. You're back. #getalife"
+        };
+
+        public static string[] BuildingRelocationMessages() => new string[]
+        {
+            "MAKE YOUR MIND UP!",
+            "We have literally the worst mayor in the world. #indecisive",
+            "Why can't we just build things in the right place to start with!?",
+            "Just when the view was getting acceptable, someone goes and moves something.",
+            "If you'd build enough medical buildings, you might be able to get something for that indecision."
+        };
+
+        public static string[] Responses() => new string[]
+        {
+            "EVERYONE PLEASE JUST BE QUIET",
+            "{previousHandle} you're talking utter nonsense, please stop.",
+            "Oh just give it a rest {previousHandle}. #tired",
+            "{previousHandle} Funny, I don't remember asking for your opinion!",
+            "{previousHandle} go back to school! Oh wait, no, the mayor still hasn't built enough :( #stupid"
+        };
+    }
+}
--- GrumpyChirperMod/Engines/GrumpyEngine.cs
+++ GrumpyChirperMod/Engines/GrumpyEngine.cs
@@ -1,3 +1,4 @@
+using GrumpyChirperMod.Data;
 using GrumpyChirperMod.Grumpiness;
 using ICities;
 using System;
@@ -8,40 +9,6 @@ namespace GrumpyChirperMod.Engines
     public class GrumpyEngine : IGrumpyEngine
     {
         private readonly Random _random;
-        private static readonly string[] Names = new string[] 
-        { 
-            "Grumpy Bob", 
-            "Dave The Grouch", 
-            "Angry Rob",
-            "Irritated Irene"
-        };
-        
-        private static readonly string[] LoadingMessages = new string[] 
-        { 
-            "Don't you have anything better to do? #sigh", 
-            "Oh good the mayor's back. #giveup", 
-            "Another beautiful day in the worst city ever!",
-            "BREAKING: The mayor unveils plans to somehow make things even worse.",
-            "Oh great. You're back. #getalife"
-        };
-
-        private static readonly string[] BuildingRelocationMessages = new string[]
-        {
-            "MAKE YOUR MIND UP!",
-            "We have literally the worst mayor in the world. #indecisive",
-            "Why can't we just build things in the right place to start with!?",
-            "Just when the view was getting acceptable, someone goes and moves something.",
-            "If you'd build enough medical buildings, you might be able to get something for that indecision."
-        };
-
-        private static readonly string[] Responses = new string[]
-        {
-            "EVERYONE PLEASE JUST BE QUIET",
-            "{previousHandle} you're talking utter nonsense, please stop.",
-            "Oh just give it a rest {previousHandle}. #tired",
-            "{previousHandle} Funny, I don't remember asking for your opinion!",
-            "{previousHandle} go back to school! Oh wait, no, the mayor still hasn't built enough :( #stupid"
-        };
 
         public GrumpyEngine(Random random)
         {
@@ -66,10 +33,10 @@ namespace GrumpyChirperMod.Engines
             switch (trigger)
             {
                 case GrumpinessTrigger.LevelLoaded:
-                    return LoadingMessages;
+                    return RandomStrings.LoadingMessages();
 
                 case GrumpinessTrigger.BuildingRelocated:
-                    return BuildingRelocationMessages;
+                    return RandomStrings.BuildingRelocationMessages();
                 default:
                     return new string[] { };
             }
@@ -77,8 +44,9 @@ namespace GrumpyChirperMod.Engines
 
         public string GetRandomName()
         {
-            var index = _random.Next(0, Names.Length);
-            return Names[index];
+            var names = RandomStrings.Names();
+            var index = _random.Next(0, names.Length);
+            return names[index];
         }
 
         public string GetRandomName(string invalidName)
@@ -94,8 +62,9 @@ namespace GrumpyChirperMod.Engines
 
         public string GetRandomResponse(IChirperMessage message)
         {
-            var index = _random.Next(0, Responses.Length);
-            var response = Responses[index];
+            var responses = RandomStrings.Responses();
+            var index = _random.Next(0, responses.Length);
+            var response = responses[index];
 
             var previousHandle = "@" + message.senderName.Replace(" ", "");
 
