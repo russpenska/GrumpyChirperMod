6b1937d481488c8b2c89b8548f7ba4e545005b9c
Simon Storfors <Simon@netboxtechnology.com>
2020-04-28 12:38:59 +0100

    Add some more random strings

--- GrumpyChirperMod/Data/RandomStrings.cs
+++ GrumpyChirperMod/Data/RandomStrings.cs
@@ -15,6 +15,10 @@
             "Drunk Steve",
             "Old Man Bob",
             "Mabel The Miserable",
+            "Pensioner Penny",
+            "Mandy Malicious",
+            "Drunk Dave",
+            "Debbie Downer"
         };
 
         public static string[] LoadingMessages() => new string[]
@@ -23,7 +27,9 @@
             "Oh good the mayor's back. #giveup",
             "Another beautiful day in the worst city ever!",
             "BREAKING: The mayor unveils plans to somehow make things even worse.",
-            "Oh great. You're back. #getalife"
+            "Oh great. You're back. #getalife",
+            "Looks like the mayor is back again from his professional golfing career!",
+            "Brb, I'm starting a betting site on how much further in debt we'll go by the end of the day"
         };
 
         public static string[] BuildingRelocationMessages() => new string[]
@@ -32,7 +38,9 @@
             "We have literally the worst mayor in the world. #indecisive",
             "Why can't we just build things in the right place to start with!?",
             "Just when the view was getting acceptable, someone goes and moves something.",
-            "If you'd build enough medical buildings, you might be able to get something for that indecision."
+            "If you'd build enough medical buildings, you might be able to get something for that indecision.",
+            "If you don't like the change you can move to another city! #MakeOurCityGreatAgain #MyMayor",
+            "New drinking game, take a shot everytime the mayor can't make their mind up #dead"
         };
 
         public static string[] Responses() => new string[]
@@ -41,7 +49,8 @@
             "{previousHandle} you're talking utter nonsense, please stop.",
             "Oh just give it a rest {previousHandle}. #tired",
             "{previousHandle} Funny, I don't remember asking for your opinion!",
-            "{previousHandle} go back to school! Oh wait, no, the mayor still hasn't built enough :( #stupid"
+            "{previousHandle} go back to school! Oh wait, no, the mayor still hasn't built enough :( #stupid",
+            "This is probably the most coheret thing this office has ever said, and it's still nonsense!"
         };
     }
 }
